eb2768f810b12abbe26e6dd97b31a4f2a7d461d2
walidbakiri-aws <bakiri.ww@gmail.com>
2025-04-12 09:04:32 +0100

    Inknhssfdfkdsdmkjlkhlsk

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import BtnAdd from "../compenent/layout/BtnAdd";
 import NavigationBar from "../compenent/layout/NavigationBar";
 import classes from "./Quiz.module.css";
@@ -12,9 +12,6 @@ import { useMediaQuery } from "react-responsive";
 import { FcPrevious } from "react-icons/fc";
 import useLocalStorage from "use-local-storage";
 function Quiz() {
-  const radioRefs = useRef([]);
-  const checkBoxAllCoursRefs = useRef([]);
-
   const refreshPage = useSignal(0);
   const [isDark, setIsDark] = useLocalStorage("isDark", false);
   const token = localStorage.getItem("tokengoat");
@@ -181,13 +178,6 @@ function Quiz() {
   //******************************************************************* */
   //*******handle Change Module**********************************************
   const handleChangeModule = (e) => {
-    /****type qcm  iniialisation************** */
-    radioRefs.current.forEach((radio) => {
-      if (radio) radio.checked = false;
-    });
-    setSelectMultipleCours("");
-
-    /****************************************** */
     setSelectMultipleCours([]); //initialiszer for check each time change module
     setMinMaxYearFinal([]);
     setMaxYearValue("");
@@ -354,30 +344,7 @@ function Quiz() {
     }
   };
   //****************************************************************************/
-  //*******handle handleChangeCours************************************************
-  const handleSelectAllCours = async (e) => {
-    //****get single cour****************************************************** */
-    if (e.target.checked) {
-      setSelectMultipleCours([
-        ...SelectedCours,
-        ...AllCours.map((course) => course.id),
-      ]);
-      checkBoxAllCoursRefs.current.forEach((radio) => {
-        if (radio) radio.checked = true;
-      });
-      setMinYearMultipleCours((minYear) => [...minYear, "2017"]);
 
-      setMaxYearMultipleCours((maxYear) => [...maxYear, "2025"]);
-    } else {
-      setSelectMultipleCours("");
-      checkBoxAllCoursRefs.current.forEach((radio) => {
-        if (radio) radio.checked = false;
-      });
-    }
-
-    //************************************************************************ */
-  };
-  //****************************************************************************** */
   //*******qcm casclinique toutes************************************************
   const handleRadioQcmType = (e) => {
     console.log(SelectedCoursCommencerBtn);
@@ -509,9 +476,12 @@ function Quiz() {
 
   //**********************get module name************************************** */
   const getModuleName = async (moduleId) => {
-    const result = await axios.get(`https://goatqcm-instance.com/module/${moduleId}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    const result = await axios.get(
+      `https://goatqcm-instance.com/module/${moduleId}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
     moduleName.value = result.data.moduleName;
   };
 
@@ -868,7 +838,7 @@ function Quiz() {
   const handleShowCours = () => {
     console.log(ExisteQcmInTous.value);
     console.log(ExisteCasClinique);
-    console.log(selectMultipleCours);
+    console.log(SelectedCours);
   };
 
   return (
@@ -881,7 +851,6 @@ function Quiz() {
             className={classes.contanerspace}
             data-theme={isDark ? "dark" : "light"}
           >
-            <button onClick={handleShowCours}>test</button>
             <div className={classes.allcards}>
               <div className={`${classes.qcmmodele} table-hover shadow`}>
                 <div
@@ -970,47 +939,26 @@ function Quiz() {
                   </div>
                   <div className={classes.childmodulecoursdiv}>
                     {VisibleParCourDiv && (
-                      <>
-                        <div className="form-check">
-                          <div className={classes.moduleitem}>
+                      <div className="form-check">
+                        {AllCours.map((cour, index) => (
+                          <div key={cour.id} className={classes.moduleitem}>
                             <input
                               className="form-check-input fs-6 "
                               type="checkbox"
                               name="flexRadioDefaultCours"
-                              onChange={handleSelectAllCours}
+                              id={cour.coursName}
+                              value={cour.id}
+                              onChange={handleChangeCours}
                             />
 
                             <h6
                               className={`${classes.typesujeth6} form-check-label `}
                             >
-                              Sélectionner Tous
+                              {cour.coursName}
                             </h6>
                           </div>
-                        </div>
-                        <div className="form-check">
-                          {AllCours.map((cour, index) => (
-                            <div key={cour.id} className={classes.moduleitem}>
-                              <input
-                                className="form-check-input fs-6 "
-                                type="checkbox"
-                                name="flexRadioDefaultCours"
-                                ref={(el) =>
-                                  (checkBoxAllCoursRefs.current[index] = el)
-                                }
-                                id={cour.coursName}
-                                value={cour.id}
-                                onChange={handleChangeCours}
-                              />
-
-                              <h6
-                                className={`${classes.typesujeth6} form-check-label `}
-                              >
-                                {cour.coursName}
-                              </h6>
-                            </div>
-                          ))}
-                        </div>
-                      </>
+                        ))}
+                      </div>
                     )}
                   </div>
                   <div className={classes.vlmodule}></div>
@@ -1036,7 +984,6 @@ function Quiz() {
                             className="form-check-input fs-6 "
                             type="radio"
                             name="typeqcm"
-                            ref={(el) => (radioRefs.current[index] = el)}
                             value={typeQcm}
                             id={typeQcm}
                             onChange={handleRadioQcmType}
@@ -1210,44 +1157,26 @@ function Quiz() {
                   {showCoursDiv && (
                     <div className={classes.childmodulecoursdivphone}>
                       {VisibleParCourDiv && (
-                        <>
-                          <div className="form-check">
-                            <div className={classes.moduleitem_phone}>
+                        <div className="form-check">
+                          {AllCours.map((cour, index) => (
+                            <div
+                              key={cour.id}
+                              className={classes.moduleitem_phone}
+                            >
                               <input
                                 className="form-check-input fs-6 "
                                 type="checkbox"
                                 name="flexRadioDefaultCours"
-                                onChange={handleSelectAllCours}
+                                id={cour.coursName}
+                                value={cour.id}
+                                onChange={handleChangeCours}
                               />
                               <label className="form-check-label fs-6 ">
-                                Selecionner Tous
+                                {cour.coursName}
                               </label>
                             </div>
-                          </div>
-                          <div className="form-check">
-                            {AllCours.map((cour, index) => (
-                              <div
-                                key={cour.id}
-                                className={classes.moduleitem_phone}
-                              >
-                                <input
-                                  className="form-check-input fs-6 "
-                                  type="checkbox"
-                                  name="flexRadioDefaultCours"
-                                  ref={(el) =>
-                                    (checkBoxAllCoursRefs.current[index] = el)
-                                  }
-                                  id={cour.coursName}
-                                  value={cour.id}
-                                  onChange={handleChangeCours}
-                                />
-                                <label className="form-check-label fs-6 ">
-                                  {cour.coursName}
-                                </label>
-                              </div>
-                            ))}
-                          </div>
-                        </>
+                          ))}
+                        </div>
                       )}
                     </div>
                   )}
