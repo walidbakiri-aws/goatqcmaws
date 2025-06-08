d7d99e0be5d40b84561b0db73e1197d2c61423e0
walidbakiriguit <bakiri.ww@gmail.com>
2025-06-08 17:48:47 +0100

    Inknhssfdfkdsdkscscsmsllqdssqsqml

---
+++ src/pages/CardResidanatSujet.jsx
@@ -0,0 +1,162 @@
+import { useEffect, useState } from "react";
+import NavigationBar from "../compenent/layout/NavigationBar";
+import classes from "./CardResidanatSujet.module.css";
+import Sidebar from "./Sidebar";
+import { useSignal } from "@preact/signals-react/runtime";
+import axios from "axios";
+import { useMediaQuery } from "react-responsive";
+import useLocalStorage from "use-local-storage";
+import { useNavigate } from "react-router-dom";
+import playsessionicon from "../compenent/layout/img/playsession.png";
+import detail from "../compenent/layout/img/detailicon.png";
+import UserService from "../compenent/layout/service/UserService";
+import ModalDetailSession from "./ModalDetailSession";
+import Backdrop from "./Backdrop";
+function CardResidanatSujet() {
+  //******SideBare Change************************************* */
+  function etatsidebare(etat) {
+    setShowSideBare(etat);
+  }
+  const [ShowSideBare, setShowSideBare] = useState(false);
+
+  /*********adresse Ip***************************** */
+  let ipAdresse = useSignal("");
+  let getUserAdresseIp = useSignal("");
+  const token = localStorage.getItem("tokengoat");
+  const userIdToken = localStorage.getItem("userId");
+  //************************************************* */
+  const isAuthenticated = UserService.isAuthenticated();
+  const isAdmin = UserService.isAdmin();
+  const isOnlyAdmin = UserService.adminOnly();
+  let navigateBoardQuiz = useNavigate();
+  useEffect(() => {}, []);
+  //******************************************************************* */
+  const [isDark, setIsDark] = useLocalStorage("isDark", false);
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
+  const isBigScreen = useMediaQuery({ query: "(min-width: 1824px)" });
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
+  const isRetina = useMediaQuery({ query: "(min-resolution: 2dppx)" });
+
+  //************************************************************************ */
+  const yearsName = [
+    "2015",
+    "2016",
+    "2017",
+    "2018",
+    "2019",
+    "2020",
+    "2021",
+    "2022",
+    "2023",
+  ];
+  const hanleOnclickYear = (getYearMed) => {
+    if (getYearMed === "2015") {
+      navigateBoardQuiz(`/shopdfresidant`, {
+        state: {
+          sjetResidant: "2015",
+        },
+      });
+    } else if (getYearMed === "2016") {
+      navigateBoardQuiz(`/shopdfresidant`, {
+        state: {
+          sjetResidant: "2016",
+        },
+      });
+    } else if (getYearMed === "2017") {
+      navigateBoardQuiz(`/shopdfresidant`, {
+        state: {
+          sjetResidant: "2017",
+        },
+      });
+    } else if (getYearMed === "2018") {
+      navigateBoardQuiz(`/shopdfresidant`, {
+        state: {
+          sjetResidant: "2018",
+        },
+      });
+    } else if (getYearMed === "2019") {
+      navigateBoardQuiz(`/shopdfresidant`, {
+        state: {
+          sjetResidant: "2019",
+        },
+      });
+    } else if (getYearMed === "2020") {
+      navigateBoardQuiz(`/shopdfresidant`, {
+        state: {
+          sjetResidant: "2020",
+        },
+      });
+    } else if (getYearMed === "2021") {
+      navigateBoardQuiz(`/shopdfresidant`, {
+        state: {
+          sjetResidant: "2021",
+        },
+      });
+    } else if (getYearMed === "2022") {
+      navigateBoardQuiz(`/shopdfresidant`, {
+        state: {
+          sjetResidant: "2022",
+        },
+      });
+    } else if (getYearMed === "2023") {
+      navigateBoardQuiz(`/shopdfresidant`, {
+        state: {
+          sjetResidant: "2023",
+        },
+      });
+    }
+  };
+  return (
+    <>
+      <NavigationBar changeetatsidebar={etatsidebare} />
+      <div className={classes.addingdiv}>
+        <div className={classes.sidebare}>{ShowSideBare && <Sidebar />}</div>
+        {isDesktopOrLaptop && (
+          <div
+            className={classes.contanerspace}
+            data-theme={isDark ? "dark" : "light"}
+          >
+            <div className={classes.quizzContainer}>
+              {yearsName.map((year, index) => (
+                <div
+                  key={index}
+                  className={classes.cardyear}
+                  onClick={() => {
+                    hanleOnclickYear(year);
+                  }}
+                >
+                  <h5 className={classes.cartitle}>Résidanat {year}</h5>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+        {isTabletOrMobile && (
+          <div
+            className={classes.contanerspace_phone}
+            data-theme={isDark ? "dark" : "light"}
+          >
+            <div className={classes.quizzContainer_phone}>
+              {yearsName.map((year, index) => (
+                <div
+                  key={index}
+                  className={classes.cardyear_phone}
+                  onClick={() => {
+                    hanleOnclickYear(year);
+                  }}
+                >
+                  <h5 className={classes.cartitle_phone}>Résidanat {year}</h5>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default CardResidanatSujet;
