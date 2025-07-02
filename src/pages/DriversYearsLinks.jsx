f2c89eac395fa6bb03f4ec416bc2e7ee412ebdae
walidbakiri-aws <bakiri.ww@gmail.com>
2025-07-02 18:20:01 +0100

    Inknhsklkjfsfdfklkjssslsdldsssllllkml*ss

--- src/pages/DriversYearsLinks.jsx
+++ src/pages/DriversYearsLinks.jsx
@@ -59,7 +59,11 @@ function DriversYearsLinks() {
     sixYear:
       "https://drive.google.com/drive/folders/1k-NZ5fBemEoD-dPMrD2fMdbJePMYO1ez?usp=drive_link",
     samediYear:
+<<<<<<< HEAD
       "https://drive.google.com/drive/folders/1MsRFY98XABNIs-4lsN-prWGzYJVE1aDs?usp=drive_link",
+=======
+      "https://drive.google.com/drive/folders/1XuR_eVGVYNoSGqYdoTfaiemJe5SZsGYl?usp=sharing",
+>>>>>>> ab82c03 (Inknhsklkjfsfdfklkjssslsdldsssss)
   };
   const yearsLinks24 = {
     firstYear:
