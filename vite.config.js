54ccf5d37d76ab9d767974f23ce3c3dda1c40d9c
walidbakiri-aws <bakiri.ww@gmail.com>
2025-07-29 16:20:39 +0100

    Ilkkjjssssslssssmmllfdfdfdcscfddsdscfdcffddmm

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite";
+/*import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-/*
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-}); */
-
+ 
+ 
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -20,5 +16,28 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ["buffer", "process"],
+  },
+  server: {
+    historyApiFallback: true, // 🔥 Enables SPA fallback on refresh
+  },
+});*/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    global: "window",
+  },
+  optimizeDeps: {
+    include: ["buffer", "process"],
+  },
+  server: {
+    historyApiFallback: true, // 🔥 Enables SPA fallback on refresh
+    proxy: {
+      "/api": {
+        target: "https://goatqcm-instance.com",
+      },
+    },
   },
 });
