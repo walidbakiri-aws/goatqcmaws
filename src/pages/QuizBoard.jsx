18726bf8445d5646b18eb557b5a5e60feace816a
walidbakiri-aws <bakiri.ww@gmail.com>
2025-09-10 17:52:52 +0100

    Ilkkjjssssslssssmmllfdfjmk

--- src/pages/QuizBoard.jsx
+++ src/pages/QuizBoard.jsx
@@ -3527,7 +3527,7 @@ function QuizBoard(props) {
                                       }}
                                     />
                                   </div>
-                                  {/* <div className={`${classes.chatgpt} `}>
+                                  <div className={`${classes.chatgpt} `}>
                                     <img
                                       src={chatgpt}
                                       height="100%"
@@ -3546,7 +3546,7 @@ function QuizBoard(props) {
                                         handleDeepSeekBtn(qcm.id);
                                       }}
                                     />
-                                  </div>*/}
+                                  </div>
                                   <div className={`${classes.note} `}>
                                     <img
                                       src={noteimage}
@@ -4432,7 +4432,7 @@ function QuizBoard(props) {
                                           }}
                                         />
                                       </div>
-                                      {/*<div
+                                      <div
                                         className={`${classes.chatgpt_phone} `}
                                       >
                                         <img
@@ -4455,7 +4455,7 @@ function QuizBoard(props) {
                                             handleDeepSeekBtn(qcm.id);
                                           }}
                                         />
-                                      </div>*/}
+                                      </div>
                                       <div className={`${classes.note_phone} `}>
                                         <img
                                           src={noteimage}
