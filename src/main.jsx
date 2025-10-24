78e0bd9e2f3068ae088e1ace4c84c5094923f016
walidbakiri-aws <bakiri.ww@gmail.com>
2025-10-24 10:38:22 +0100

    Ilkkjjssssslmlldllmmmkkkkkk

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter } from "react-router-dom";
-
+import { HashRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 /*root.render(
   <BrowserRouter>
