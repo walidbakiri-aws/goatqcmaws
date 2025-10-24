da6d3030f60eeb2765967751030ce5bd6c212da2
walidbakiri-aws <bakiri.ww@gmail.com>
2025-10-24 16:27:34 +0100

    Ilkkjjssssslmlldllmmmkkkdssdmm

--- src/pages/ChatGptfinal.jsx
+++ src/pages/ChatGptfinal.jsx
@@ -40,15 +40,25 @@ function ChatGptfinal(props) {
 
   const API_KEY_1 =
     "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";
-  const API_KEY_2 = import.meta.env.VITE_OPENAI_API_KEY_CHAT_2;
-  const API_KEY_3 = import.meta.env.VITE_OPENAI_API_KEY_CHAT_3;
-  const API_KEY_4 = import.meta.env.VITE_OPENAI_API_KEY_CHAT_4;
-  const API_KEY_5 = import.meta.env.VITE_OPENAI_API_KEY_CHAT_5;
-  const API_KEY_6 = import.meta.env.VITE_OPENAI_API_KEY_CHAT_6;
-  const API_KEY_7 = import.meta.env.VITE_OPENAI_API_KEY_CHAT_7;
-  const API_KEY_8 = import.meta.env.VITE_OPENAI_API_KEY_CHAT_8;
-  const API_KEY_9 = import.meta.env.VITE_OPENAI_API_KEY_CHAT_9;
-  const API_KEY_10 = import.meta.env.VITE_OPENAI_API_KEY_CHAT_10;
+  const API_KEY_2 =
+    "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";
+  const API_KEY_3 =
+    "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";
+  const API_KEY_4 =
+    "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";
+  const API_KEY_5 =
+    "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";
+  const API_KEY_6 =
+    "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";
+  const API_KEY_7 =
+    "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";
+  const API_KEY_8 =
+    "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";
+  const API_KEY_9 =
+    "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";
+  const API_KEY_10 =
+    "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";
+
   const systemMessage = { role: "system", content: "" };
 
   // ------------------ SIDEBAR ------------------
