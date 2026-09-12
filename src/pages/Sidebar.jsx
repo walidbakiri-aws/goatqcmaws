9f096b52308d771b9fcb67bbcb7e923f7edae278
walidbakiri-aws <bakiri.ww@gmail.com>
2026-09-12 20:09:32 +0100

    Ilgfcgfcvhbkjkljkkkkdm

--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -172,17 +172,7 @@ function Sidebar() {
     getAllQcmCasCliniqueSaves();
 
     testVisiteurUser();
-
-    if (ModalExtraIsOpen) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "";
-    }
-
-    return () => {
-      document.body.style.overflow = "";
-    };
-  }, [ModalExtraIsOpen]);
+  }, []);
 
   // ******************************************************************
   // EXTRA ACCESS
